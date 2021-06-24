8d428ca8faef8ab51a1c427c30f364c18b3cd6b6
Lalit <lalitvijay9480@gmail.com>
2021-06-24 13:57:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import 'nextra-theme-docs/style.css'
+import Head from 'next/head'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <base href="https://staticshield-docs.vercel.app/" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
