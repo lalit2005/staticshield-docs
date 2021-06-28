1d60ec85b69aa768a72d01993856f57aefa1e13d
Lalit <lalitvijay9480@gmail.com>
2021-06-28 23:11:26 +0530

    chore: fix video embed

---
+++ components/NextjsDemo.js
@@ -0,0 +1,14 @@
+export default function NextjsDemo() {
+  return (
+    <div style="position: relative; padding-bottom: 62.5%; height: 0;">
+      <iframe
+        src="https://www.loom.com/embed/d591d8f7bc784fc89240993e660f6898"
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+      ></iframe>
+    </div>
+  )
+}
